26e24a5d22c783bd940054b0e93f5059b4704e77
Pooi lai Choong <932400284@bun-211-14.local>
2019-10-24 11:58:02 +1300

    updated

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ var houses = [
   rent  : 190,
   bedroom : 4,
   bathroom  : 2,
-  parking : true,
-  furnishing  : true,
-  couple  : false,
+  parking : 'yes',
+  furnishing  : 'yes',
+  couple  : 'no',
   availability  : '13 Nov 2019',
   photo : 'images/house1.jpg'
   },
@@ -20,9 +20,9 @@ var houses = [
   rent  : 240,
   bedroom : 3,
   bathroom  : 1,
-  parking : true,
-  furnishing  : true,
-  couple  : true,
+  parking : 'yes',
+  furnishing  : 'yes',
+  couple  : 'yes',
   availability  : '21 Oct 2019',
   photo : 'images/house2.jpg'
   },
@@ -33,9 +33,9 @@ var houses = [
   rent  : 300,
   bedroom : 4,
   bathroom  : 2,
-  parking : false,
-  furnishing  : true,
-  couple  : true,
+  parking : 'no',
+  furnishing  : 'yes',
+  couple  : 'yes',
   availability  : '02 Nov 2019',
   photo : 'images/house3.jpg'
   },
@@ -46,35 +46,170 @@ var houses = [
   rent  : 200,
   bedroom : 3,
   bathroom  : 1,
-  parking : false,
-  furnishing  : false,
-  couple  : false,
+  parking : 'no',
+  furnishing  : 'no',
+  couple  : 'no',
   availability  : '03 Nov 2019',
   photo : 'images/house4.jpg'
-  }
+  },
 
+  {
+  name  : 'Mount Victoria, 3 bedrooms',
+  address : 'Mount Victoria, Wellington',
+  rent  : 204,
+  bedroom : 3,
+  bathroom  : 1,
+  parking : 'no',
+  furnishing  : 'no',
+  couple  : 'no',
+  availability  : '19 Nov 2019',
+  photo : 'images/house5.jpg'
+  },
+
+  {
+  name  : 'Newlands, 3 bedrooms',
+  address : 'Newlands, Wellington',
+  rent  : 180,
+  bedroom : 3,
+  bathroom  : 1,
+  parking : 'no',
+  furnishing  : 'no',
+  couple  : 'no',
+  availability  : '21 Oct 2019',
+  photo : 'images/house6.jpg'
+  },
+
+  {
+  name  : 'Kilbirnie, 3 bedrooms',
+  address : 'Kilbirnie, Wellington',
+  rent  : 220,
+  bedroom : 3,
+  bathroom  : 1,
+  parking : 'no',
+  furnishing  : 'yes',
+  couple  : 'no',
+  availability  : '03 Nov 2019',
+  photo : 'images/house7.jpg'
+  },
+
+  {
+  name  : 'Newlands, 3 bedrooms',
+  address : 'Newlands, Wellington',
+  rent  : 180,
+  bedroom : 3,
+  bathroom  : 1,
+  parking : 'no',
+  furnishing  : 'no',
+  couple  : 'no',
+  availability  : '21 Oct 2019',
+  photo : 'images/house8.jpg'
+  },
+
+  {
+  name  : 'Island Bay,  bedroom',
+  address : 'Island Bay, Wellington',
+  rent  : 330,
+  bedroom : 1,
+  bathroom  : 1,
+  parking : 'yes',
+  furnishing  : 'no',
+  couple  : 'no',
+  availability  : '16 Oct 2019',
+  photo : 'images/house9.jpg'
+  },
+
+  {
+  name  : 'Newlands, 2 bedrooms',
+  address : 'Newlands, Wellington',
+  rent  : 295,
+  bedroom : 2,
+  bathroom  : 1,
+  parking : 'yes',
+  furnishing  : 'no',
+  couple  : 'no',
+  availability  : '02 Nov 2019',
+  photo : 'images/house10.jpg'
+  }
 
 ];
 
 function allHouses(){
 for (var i=0; i<houses.length; i++) {
   document.getElementById('houses').innerHTML
-   += '</br><div class="card" style="width: 18rem;">'
-   + '<img class="img-thumbnail" src=" ' + houses[i].photo + ' " alt="House"/>'
+   += '<div class="row">'
+   + '<div class="col">'
+   + '</br><div class="card" style="width: 23rem;">'
+   += '<img class="img-thumbnail imgSize" src=" ' + houses[i].photo + ' " alt="House"/>'
    + '<div class="card-body">'
-   + '<h5 class="card-title">' + houses[i].name + '</h5>'
-   + '</br> Address: <p> ' + houses[i].address + ' </p>'
-   + '</br> Rent / week : <p> ' + houses[i].rent + ' </p>'
-   + '</br> Bedrooms : <p> ' + houses[i].bedroom + ' </p>'
-   + '</br> Bathrooms : <p> ' + houses[i].bathroom + ' </p>'
-   + '</br> Parking : <p> ' + houses[i].parking + ' </p>'
-   + '</br> Furnishing : <p> ' + houses[i].furnishing + ' </p>'
-   + '</br> Couple : <p> ' + houses[i].couple + ' </p>'
-   + '</br> Availability : <p> ' + houses[i].availablity + ' </p>'
+   + '<h3 class="card-title text-danger">' + houses[i].name + '</h3>'
+   + '<h5> Address:  ' + houses[i].address + ' </h5>'
+   + '<h5> Rent / week : $' + houses[i].rent + ' </h5>'
+   + '<h5> Bedrooms :  ' + houses[i].bedroom + ' </h5>'
+   + '<h5> Bathrooms :  ' + houses[i].bathroom + ' </h5>'
+   + '<h5> Parking :  ' + houses[i].parking + ' </h5>'
+   + '<h5> Furnishing :  ' + houses[i].furnishing + ' </h5>'
+   + '<h5> Couple :  ' + houses[i].couple + ' </h5>'
+   + '<h5> Availability :  ' + houses[i].availablity + ' </h5>'
+   + '</div>'
+   + '</div>'
    + '</div>'
    + '</div>';
+ }
 }
-}
+
+document.getElementById('showCouple').addEventListener('click', function(){
+  document.getElementById('houses').innerHTML = '';
+  for (var i = 0; i < houses.length; i++) {
+    if (houses[i].couple === "yes") {
+      document.getElementById('houses').innerHTML
+      += '<div class="row">'
+      + '<div class="col">'
+      + '</br><div class="card" style="width: 23rem;">'
+      + '<img class="img-thumbnail imgSize" src=" ' + houses[i].photo + ' " alt="House"/>'
+      + '<div class="card-body">'
+      + '<h3 class="card-title text-danger">' + houses[i].name + '</h3>'
+      + '<h5> Address:  ' + houses[i].address + ' </h5>'
+      + '<h5> Rent / week : $' + houses[i].rent + ' </h5>'
+      + '<h5> Bedrooms :  ' + houses[i].bedroom + ' </h5>'
+      + '<h5> Bathrooms :  ' + houses[i].bathroom + ' </h5>'
+      + '<h5> Parking :  ' + houses[i].parking + ' </h5>'
+      + '<h5> Furnishing :  ' + houses[i].furnishing + ' </h5>'
+      + '<h5> Couple :  ' + houses[i].couple + ' </h5>'
+      + '<h5> Availability :  ' + houses[i].availablity + ' </h5>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>';
+    }
+  }
+});
+
+document.getElementById('showParking').addEventListener('click', function(){
+  document.getElementById('houses').innerHTML = '';
+  for (var i = 0; i < houses.length; i++) {
+    if (houses[i].parking === "yes") {
+      document.getElementById('houses').innerHTML
+      += '<div class="row">'
+      + '<div class="col">'
+      + '</br><div class="card" style="width: 23rem;">'
+      + '<img class="img-thumbnail imgSize" src=" ' + houses[i].photo + ' " alt="House"/>'
+      + '<div class="card-body">'
+      + '<h3 class="card-title text-danger">' + houses[i].name + '</h3>'
+      + '<h5> Address:  ' + houses[i].address + ' </h5>'
+      + '<h5> Rent / week : $' + houses[i].rent + ' </h5>'
+      + '<h5> Bedrooms :  ' + houses[i].bedroom + ' </h5>'
+      + '<h5> Bathrooms :  ' + houses[i].bathroom + ' </h5>'
+      + '<h5> Parking :  ' + houses[i].parking + ' </h5>'
+      + '<h5> Furnishing :  ' + houses[i].furnishing + ' </h5>'
+      + '<h5> Couple :  ' + houses[i].couple + ' </h5>'
+      + '<h5> Availability :  ' + houses[i].availablity + ' </h5>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>';
+    }
+  }
+});
 
 document.getElementById('showHouses').addEventListener('click', function(){
   allHouses();
